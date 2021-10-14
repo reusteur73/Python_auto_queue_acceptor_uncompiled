6fc31d402f05250cb6b7d69678be90edb19bcbf0
reusteur73 <92535962+reusteur73@users.noreply.github.com>
2021-10-15 02:36:38 +1100

    Add files via upload

---
+++ accept.py
@@ -0,0 +1,31 @@
+from time import sleep
+import pyautogui
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+
+
+def click(x, y):
+    pyautogui.click(x, y)
+
+
+def check_screen():
+    button = pyautogui.locateOnScreen('C:/Users/maloa/Documents/Python/queue_acceptor/en.png', confidence=0.7)
+    buttonPt = pyautogui.locateOnScreen('C:/Users/maloa/Documents/Python/queue_acceptor/pt.png', confidence=0.7)
+
+    if button != None:
+        click(button.left, button.top)
+        return True
+
+    if buttonPt != None:
+        click(buttonPt.left, buttonPt.top)
+        return True
+
+    return False
+print('Waiting for match...')
+while True:
+    if check_screen():
+        print('Match accepted.')
+        sleep(6)
+
---
+++ run.cmd
@@ -0,0 +1,2 @@
+cd PATH_TO_PYTHON3.6_FOLDER
+python.exe PATH_TO_accept.py
