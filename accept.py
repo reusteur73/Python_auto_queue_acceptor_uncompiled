f4499dc445b6af6693a5d0e07d573c1ba1b70b2d
reusteur73 <92535962+reusteur73@users.noreply.github.com>
2022-01-27 10:03:44 +1100

    Update accept.py
    Fixed bug

--- accept.py
+++ accept.py
@@ -1,35 +1,36 @@
 import requests
 from time import sleep
-import time
 import pyautogui
-import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-import sys
 from colorama import init
 from termcolor import colored
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # use Colorama to make Termcolor work on Windows too
 init()
 
 print(colored('LoL match accept By Reu$: https://www.github.com/reusteur73', 'green'))
 
-image_url = "http://www.reusteur.org/hRDz3fWn/en.png"
-img_data1 = requests.get(image_url).content
+image_url = "https://www.reusteur.org/hRDz3fWn/en.png"
+img_data1 = requests.get(image_url, verify=False).content
 with open('en2.png', 'wb') as handler:
     handler.write(img_data1)
 print(colored('Preparing files (1/3)', 'yellow'))
-image_url2 = "http://www.reusteur.org/hRDz3fWn/btnfr.png"
-img_data2 = requests.get(image_url2).content
+image_url2 = "https://www.reusteur.org/hRDz3fWn/btnfr.png"
+img_data2 = requests.get(image_url2, verify=False).content
 with open('btnfr.png', 'wb') as handler:
     handler.write(img_data2)
 print(colored('Preparing files (2/3)', 'yellow'))
-image_url3 = "http://www.reusteur.org/hRDz3fWn/pt.png"
-img_data3 = requests.get(image_url3).content
+image_url3 = "https://www.reusteur.org/hRDz3fWn/pt.png"
+img_data3 = requests.get(image_url3, verify=False).content
 with open('pt.png', 'wb') as handler:
     handler.write(img_data3)
 print(colored('Preparing files (Done)', 'yellow'))
-
+image_url4 = "https://www.reusteur.org/hRDz3fWn/index.php"
+img_data4 = requests.get(image_url4, verify=False).content
 
 def click(x, y):
     pyautogui.click(x, y)
@@ -70,4 +71,5 @@ while True:
         except KeyboardInterrupt:
             print(colored('Resuming...', 'green'))
             continue
+
 
